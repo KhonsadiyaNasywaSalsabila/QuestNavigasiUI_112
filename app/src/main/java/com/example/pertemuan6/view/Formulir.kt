00d0af7aeb6e234c566904477474f8b7dc084dd6
KhonsadiyaNasywaSalsabila <nasywa9905@gmail.com>
2025-11-06 15:36:36 +0700

    Fungsi Form Isian

---
+++ app/src/main/java/com/example/pertemuan6/view/Formulir.kt
@@ -0,0 +1,11 @@
+package com.example.pertemuan6.view
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun FormIsian(
+    jenisK:List<String> = listOf("Laki-laki", "Perempuan"),
+    OnSubmitBtnClick : () -> Unit
+){
+
+}
