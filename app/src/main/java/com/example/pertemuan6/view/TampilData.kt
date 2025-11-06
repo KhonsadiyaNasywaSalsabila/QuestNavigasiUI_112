313a0d368c0bc3f9c650db211a6eaabf82ee36f4
KhonsadiyaNasywaSalsabila <nasywa9905@gmail.com>
2025-11-06 18:58:36 +0700

    Tampilkan data statis (Nama, Jenis Kelamin, Alamat) di Column

--- app/src/main/java/com/example/pertemuan6/view/TampilData.kt
+++ app/src/main/java/com/example/pertemuan6/view/TampilData.kt
@@ -61,10 +61,23 @@ fun TampilData(
             Column(
                 modifier =  Modifier.padding(dimensionResource(id=R.dimen.padding_small))
             ){
-                HorizontalDivider(thickness = 1.dp, color = Color.Cyan)
-
+                items.forEach { item ->
+                    Column {
+                        Text(
+                            text = item.first.uppercase(),
+                            fontSize = 16.sp
+                        )
+                        Text(
+                            text = item.second,
+                            fontWeight = FontWeight.Bold,
+                            fontFamily = FontFamily.Cursive, fontSize = 22.sp
+                        )
+                    }
+                    HorizontalDivider(thickness = 1.dp, color = Color.Cyan)
+                }
+                Spacer(modifier = Modifier.height(10.dp))
             }
-            Spacer(modifier = Modifier.height(10.dp))
+
 
         }
 
