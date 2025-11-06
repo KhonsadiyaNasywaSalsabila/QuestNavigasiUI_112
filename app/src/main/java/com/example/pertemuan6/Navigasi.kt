3c1742425ee643a6938e6d7c10372caf44cb101f
KhonsadiyaNasywaSalsabila <nasywa9905@gmail.com>
2025-11-06 21:37:59 +0700

    Terapkan navigasi kembali (back) dari Detail ke Formulirku

--- app/src/main/java/com/example/pertemuan6/Navigasi.kt
+++ app/src/main/java/com/example/pertemuan6/Navigasi.kt
@@ -39,13 +39,21 @@ fun DataApp(
             composable(route = Navigasi.Detail.name) {
                 TampilData(
                     onBackBtnClick = {
+                        cancelAndBackToFormulirku(navController)
 
                     }
                 )
             }
 
         }
+
     }
 }
 
+private fun cancelAndBackToFormulirku(
+    navController: NavHostController
+){
+    navController.popBackStack(Navigasi.Formulirku.name,
+        inclusive = false)
+}
 
